60c68beedf5200e6eb533b5ed6d349746cbdaf5a
blockmaxbu <btcdev@hotmail.com>
2025-01-28 21:58:07 +0800

    add oracle function

--- src/libs/OracleLib.sol
+++ src/libs/OracleLib.sol
@@ -2,8 +2,35 @@
 pragma solidity ^0.8.24;
 
 import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import { ConfigHelper } from "script/ConfigHelper.s.sol";
 
 
 contract OracleLib {
-    
+    AggregatorV3Interface internal dataFeed;
+
+    /**
+     * Network: Sepolia
+     * Aggregator: BTC/USD
+     * Address: 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43
+     */
+    constructor(ConfigHelper.NetConfig memory config) {
+        dataFeed = AggregatorV3Interface(
+            0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43
+        );
+    }
+
+    /**
+     * Returns the latest answer.
+     */
+    function getChainlinkDataFeedLatestAnswer() public view returns (int) {
+        // prettier-ignore
+        (
+            /* uint80 roundID */,
+            int answer,
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) = dataFeed.latestRoundData();
+        return answer;
+    }
 }
